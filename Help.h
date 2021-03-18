62afe860826b912edfc895aebc332b6f04f52da4
solutionman <solutionman@inbox.ru>
2021-03-18 12:42:52 +0700

    Help

---
+++ Help.h
@@ -0,0 +1,10 @@
+#ifndef TED_HELP_H
+#define TED_HELP_H
+#include <string>
+
+class Help{
+public:
+    static void getHelp(int argc, char **argv, std::string &fileName);
+};
+
+#endif //TED_HELP_H
--- Ted.cpp
+++ Ted.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "Help.h"
 
 int main(int argc, char **argv) {
+    std::string fileName;
+    Help::getHelp(argc, argv, fileName);
     return 0;
 }
