1dad9d0d630a055e20473993d0a0c85b7f6f0fd5
solutionman <solutionman@inbox.ru>
2021-03-23 13:00:08 +0700

    help

--- Help.cpp
+++ Help.cpp
@@ -3,8 +3,9 @@
 
 void Help::getHelp(int argc, char **argv, std::string &fileName) {
     if (argc == 1 || (fileName == "-h" || fileName == "--h" || fileName == "-help" || fileName == "--help")) {
-        std::cout << "usage: "
-                     "enter the name of file" << std::endl;
+        std::cout << "usage: \n"
+                     "enter the name of file\n"
+                     "to quit enter :q" << std::endl;
         std::exit(0);
     }
 }
