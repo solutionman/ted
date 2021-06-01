b684dac53e0dadaf32e00717561b44fbb435e2d9
solutionman <solutionman@inbox.ru>
2021-06-01 08:18:37 +0700

    help

--- Help.cpp
+++ Help.cpp
@@ -7,6 +7,7 @@ void Help::getHelp(int argc, char **argv, std::string &fileName) {
                      "enter the name of file\n"
                      "print and press enter to add text to file\n"
                      "to delete line enter :d\n"
+                     "to replace line enter :r\n"
                      "to quit enter :q" << std::endl;
         std::exit(0);
     }
